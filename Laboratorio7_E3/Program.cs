d1e6e8d516c27926853f78d263013b6cd600ccc5
alexi <alexi@ALEXANDER>
2023-10-04 17:00:58 -0500

    Agregar archivos de proyecto.

---
+++ Laboratorio7_E3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio7_E3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int año = 1900;
+
+            Console.WriteLine("Años bisiestos desde 1900 hasta 2023: ");
+
+            while (año <= 2023)
+            {
+                if (año % 4 == 0 && año % 100 != 0 || año % 400 == 0)
+                {
+                    Console.WriteLine(año);
+                }
+                año++;
+            }
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
